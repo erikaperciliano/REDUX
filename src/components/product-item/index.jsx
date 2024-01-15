b3faf4af5e2055489057601c0d102bf27b5e036e
Erika Perciliano <erikaperciliano.developer@gmail.com>
2024-01-15 09:47:54 +0000

    creates handleProductClick function to do the dispatch

--- src/components/product-item/index.jsx
+++ src/components/product-item/index.jsx
@@ -2,6 +2,8 @@ import { BsCartPlus } from "react-icons/bs";
 
 // Components
 import CustomButton from "../custom-button/index";
+import { useDispatch } from 'react-redux'
+import { addProductToCart } from '../../redux/cart/action'
 
 // Styles
 import * as Styles from "./styles";
@@ -9,10 +11,16 @@ import * as Styles from "./styles";
 // Utilities
 
 const ProductItem = ({ product }) => {
+  const dispatch = useDispatch()
+
+  const handleProductClick = () => {
+    dispatch(addProductToCart(product))
+  }
+
   return (
     <Styles.ProductContainer>
       <Styles.ProductImage imageUrl={product.imageUrl}>
-        <CustomButton startIcon={<BsCartPlus />}>
+        <CustomButton startIcon={<BsCartPlus />} onClick={handleProductClick}>
           Adicionar ao carrinho
         </CustomButton>
       </Styles.ProductImage>
