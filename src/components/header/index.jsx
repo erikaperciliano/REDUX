12938462147dd56a9eb43d79f3033904480437de
Erika Perciliano <erikaperciliano.developer@gmail.com>
2024-01-15 10:54:08 +0000

    shows at header the quantity of the products that were added at cart

--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useMemo, useState } from "react";
 import { useSelector, useDispatch } from 'react-redux'
 // Components
 import Cart from "../cart/index";
@@ -11,9 +11,13 @@ function Header() {
   const [cartIsVisible, setCartIsVisible] = useState(false);
 
   const { currentUser } = useSelector((rootReducer) => rootReducer.userReducer)
+  const { products } = useSelector((rootReducer) => rootReducer.cartReducer)
+
   const dispatch = useDispatch()
 
-  console.log({currentUser})
+  const productsCount = useMemo(() => {
+    return products.reduce((acc, curr) => acc + curr.quantity, 0)
+  },[products])
 
   const handleCartClick = () => {
     setCartIsVisible(true);
@@ -36,7 +40,7 @@ function Header() {
           <div onClick={handleLogoutClick}>Sign out</div> :
           <div onClick={handleLoginClick}>Login</div>
         }
-        <div onClick={handleCartClick}>Carrinho</div>
+        <div onClick={handleCartClick}>Carrinho ({productsCount})</div>
       </Styles.Buttons>
 
       <Cart isVisible={cartIsVisible} setIsVisible={setCartIsVisible} />
